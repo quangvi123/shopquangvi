57240c298a11a08fdff8812472b84ced7ec4d1a0
quang vi <phamquangvi10a10@gmail.com>
2025-02-09 03:43:58 +0700

    update code

--- src/components/pages/product-detail.jsx
+++ src/components/pages/product-detail.jsx
@@ -10,32 +10,32 @@ export default function ProductDetail (){
       {/* Hình ảnh sản phẩm */}
       <div className="w-full md:w-1/2 px-4 mb-8">
         <img
-          src="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHwxfHxoZWFkcGhvbmV8ZW58MHwwfHx8MTcyMTMwMzY5MHww&ixlib=rb-4.0.3&q=80&w=1080"
+          src="https://cigadesign.com/cdn/shop/files/20230314114951.jpg?v=1685622142"
           alt="Sản phẩm"
           className="w-full h-auto rounded-lg shadow-md mb-4"
           id="mainImage"
         />
         <div className="flex gap-4 py-4 justify-center overflow-x-auto">
           <img
-            src="https://images.unsplash.com/photo-1505751171710-1f6d0ace5a85?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHwxMnx8aGVhZHBob25lfGVufDB8MHx8fDE3MjEzMDM2OTB8MA&ixlib=rb-4.0.3&q=80&w=1080"
+            src="https://cigadesign.com/cdn/shop/files/20230314115022.jpg?v=1685622137"
             alt="Hình thu nhỏ 1"
             className="size-16 sm:size-20 object-cover rounded-md cursor-pointer opacity-60 hover:opacity-100 transition duration-300"
             onclick="changeImage(this.src)"
           />
           <img
-            src="https://images.unsplash.com/photo-1484704849700-f032a568e944?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw0fHxoZWFkcGhvbmV8ZW58MHwwfHx8MTcyMTMwMzY5MHww&ixlib=rb-4.0.3&q=80&w=1080"
+            src="https://cigadesign.com/cdn/shop/files/20230314115007.jpg?v=1685622137"
             alt="Hình thu nhỏ 2"
             className="size-16 sm:size-20 object-cover rounded-md cursor-pointer opacity-60 hover:opacity-100 transition duration-300"
             onclick="changeImage(this.src)"
           />
           <img
-            src="https://images.unsplash.com/photo-1496957961599-e35b69ef5d7c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw4fHxoZWFkcGhvbmV8ZW58MHwwfHx8MTcyMTMwMzY5MHww&ixlib=rb-4.0.3&q=80&w=1080"
+            src="https://cigadesign.com/cdn/shop/files/20230314115019.jpg?v=1699951405"
             alt="Hình thu nhỏ 3"
             className="size-16 sm:size-20 object-cover rounded-md cursor-pointer opacity-60 hover:opacity-100 transition duration-300"
             onclick="changeImage(this.src)"
           />
           <img
-            src="https://images.unsplash.com/photo-1528148343865-51218c4a13e6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHwzfHxoZWFkcGhvbmV8ZW58MHwwfHx8MTcyMTMwMzY5MHww&ixlib=rb-4.0.3&q=80&w=1080"
+            src="https://cigadesign.com/cdn/shop/products/ad216c653fe282092b89968bc303566f.jpg?v=1685622142"
             alt="Hình thu nhỏ 4"
             className="size-16 sm:size-20 object-cover rounded-md cursor-pointer opacity-60 hover:opacity-100 transition duration-300"
             onclick="changeImage(this.src)"
@@ -44,7 +44,7 @@ export default function ProductDetail (){
       </div>
       {/* Chi tiết sản phẩm */}
       <div className="w-full md:w-1/2 px-4">
-        <h2 className="text-3xl font-bold mb-2">Tai Nghe Không Dây Cao Cấp</h2>
+        <h2 className="text-3xl font-bold mb-2">Đồng hồ nữ thời trang</h2>
         <p className="text-gray-600 mb-4">Mã sản phẩm: WH1000XM4</p>
         <div className="mb-4">
           <span className="text-2xl font-bold mr-2">8.199.000₫</span>
